e644c64285535b018a85e5e6dc1864ede7e967e0
hhniao <448455556@qq.com>
2021-07-06 09:39:20 +0800

    注释

--- src/Utils/Signature.php
+++ src/Utils/Signature.php
@@ -15,14 +15,15 @@ class Signature
     /**
      * 校验签名.
      *
-     * @param array $data $_REQUEST 并且包含 ['uri' => $uri]
+     * @param array  $data $_REQUEST 并且包含 ['uri' => $uri]
+     * @param string $secret
      *
      * @return bool
      * @throws Exception
      * @author liuchunhua<448455556@qq.com>
      * @date   2021/5/25
      */
-    public static function checkSign($data, $secret)
+    public static function checkSign(array $data, string $secret): bool
     {
         if (!isset($data['sign'])) {
             throw new Exception('签名必须.');
@@ -34,7 +35,16 @@ class Signature
         return md5($str) === $sign;
     }
 
-    public static function buildSignString($data, $secret)
+    /**
+     * @param array  $data
+     * @param string $secret
+     *
+     * @return string
+     * @throws Exception
+     * @author liuchunhua<448455556@qq.com>
+     * @date   2021/7/6
+     */
+    public static function buildSignString(array $data, string $secret): string
     {
         if (!isset($data['timestamp'])) {
             throw new Exception('时间戳必须.');
@@ -50,7 +60,7 @@ class Signature
 
         $str = '';
 
-        foreach ($data as $k=>$v) {
+        foreach ($data as $k => $v) {
             $str .= $k . '=' . $v . '&';
         }
         $str = trim($str, '&');
