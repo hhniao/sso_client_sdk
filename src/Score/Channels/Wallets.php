bed6be2f097f51bdde68f1066be4e3f1a11e116b
hhniao <liux15658@163.com>
2023-02-28 10:38:23 +0800

    钱包

--- src/Score/Channels/Wallets.php
+++ src/Score/Channels/Wallets.php
@@ -2,35 +2,51 @@
 
 namespace App\Services\Score\Channels;
 
+use App\Models\UserWallet;
 use App\Models\UserWalletLog;
 use App\Services\Score\Deposit;
 use Exception;
 use Illuminate\Support\Facades\DB;
 
-class Wallets implements ChannelInterface
+class Wallets extends ChannelBase implements ChannelInterface
 {
-    private $name = '钱包';
+    protected $name = '钱包';
+    /**
+     * @var UserWallet
+     */
+    private $wallet;
 
     public function name()
     {
         return $this->name;
     }
 
+    public function init(Deposit $deposit)
+    {
+        $wallet = UserWallet::query()->where('currency', $deposit->currency)->first();
+
+        if (!$wallet || $wallet->amount < $deposit->cashAmount * $wallet->exchangeRate()) {
+            abort(400, '余额不足.');
+        }
+        $this->wallet = $wallet;
+    }
+
     public function exec(Deposit $deposit)
     {
+
         DB::beginTransaction();
         try {
-            $beforeAmount = $deposit->wallet->amount;
-            $amount = $deposit->cashAmount * $deposit->wallet->exchangeRate();
-            $deposit->wallet->amount -= $amount;
-            $deposit->wallet->save();
+            $beforeAmount = $this->wallet->amount;
+            $amount = $deposit->cashAmount * $this->wallet->exchangeRate();
+            $this->wallet->amount -= $amount;
+            $this->wallet->save();
 
             $log = new UserWalletLog();
             $log->amount = $amount;
             $log->order_no = $deposit->userScoreLog->order_no;
             $log->before_amount = $beforeAmount;
-            $log->currency = $deposit->wallet->currency;
-            $log->user_id = $deposit->wallet->user_id;
+            $log->currency = $this->wallet->currency;
+            $log->user_id = $this->wallet->user_id;
             $log->status = 3;
             $log->mark = '余额充值积分';
             $log->save();
